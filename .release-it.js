d94efd0cf253537a5fbe846c3c5b601d4bcd85d1
Marco Junior MBELLA <mbellamarcojunior@gmail.com>
2024-05-02 00:20:06 +0200

    fix(release): add release-it config file

---
+++ .release-it.js
@@ -0,0 +1,30 @@
+const version = '${version}';
+const name = require('./package.json').name;
+
+module.exports = {
+  git: {
+    requireBranch: 'main',
+    tagName: `v${version}`,
+    commitMessage: `chore(release): Bumping ${name} to version v${version}`,
+    requireCommits: true,
+    requireCommitsFail: false,
+    requireCleanWorkingDir: false,
+  },
+  hooks: {
+    'before:init': [
+      'git pull',
+      'npm run format',
+      'npm run lint',
+      'npm run test',
+    ],
+    'after:bump': 'npx auto-changelog -p',
+  },
+  npm: {
+    publish: false,
+  },
+  github: {
+    release: true,
+    releaseName: `${name}-v${version}`,
+    autoGenerate: true,
+  },
+};
