5766e5b89cc82c37d5f9aad48fe677e2d9d5cd68
Marco Junior MBELLA <mbellamarcojunior@gmail.com>
2024-05-02 14:18:10 +0200

    fix(lib): update lib utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,25 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { IconName } from "@/core/interfaces/icon.interface";
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getIcon = (text: string): IconName => {
+  switch (text.toLowerCase()) {
+    case "dashboard":
+      return "layout-dashboard";
+    case "analytics":
+      return "line-chart";
+    case "settings":
+      return "settings";
+    case "tokens":
+      return "dollar-sign";
+    default:
+      return "home";
+  }
+};
+
+export const fetcher = (...args: Parameters<typeof fetch>) =>
+  fetch(...args).then((res) => res.json());
